3c80f40fe80db7eaa7364bce7ac020db0fec8f24
omkar lolge <omkarlolge119.ol@gmail.com>
2022-12-31 16:03:57 +0530

    features added

--- src/components/SignIn.js
+++ src/components/SignIn.js
@@ -43,6 +43,8 @@ export default function SignIn() {
     <div className="flex h-auto items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="w-full max-w-md space-y-8">
         <div>
+          <h1 className="text-slate-500">Admin Account: username- tharun@gmail.com ; password- passwordtharun</h1>
+          <h1 className="text-slate-500">User Account: username- varun@gmail.com ; password- passwordvarun</h1>
           <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
             Sign In to your account
           </h2>
